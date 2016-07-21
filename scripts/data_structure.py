6b8f132d95a1ab9220cf1cb7a5da9499675df218
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-07-21 20:59:21 +0100

    Contains a list with the order to sort the data set .

---
+++ scripts/data_structure.py
@@ -0,0 +1,5 @@
+cols = ['players_list', 'seed', 'parameter', 'player_name', 'player_index',
+        'degree', 'neighbors', 'neighborhood_size', 'ranking', 'scores',
+        'normalised_scores',  'average_score', 'neighbors_scores',
+        'average_neighboorhood_score', 'R', 'P', 'S', 'T',
+        'clustering', 'connectivity', 'cliques']
