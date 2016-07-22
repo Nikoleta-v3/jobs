5cab50adc6d927231750587631e7e51cdfa33bd4
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-07-22 12:44:58 +0100

    Script and pbs file for simple tournaments  used in chapter three. Such as cycle and lattice with players randomly allocated to the nodes and then shuffled for a given time of repitations.

---
+++ scripts/Simple_topology.py
@@ -0,0 +1,43 @@
+import axelrod as axl
+import networkx as nx
+import random
+
+import pandas as pd
+import numpy as np
+
+from functions import *
+from data_structure import *
+
+# parameters
+turns = 200
+repetitions = 100
+ordinary_players = [s() for s in axl.ordinary_strategies]
+
+# where to export
+write_out = '/home/c1569433/src/jobs/data/Cycle.csv'
+
+results = pd.DataFrame()
+for seed in range(0, 100):
+
+    np.random.seed(seed)
+
+    # define the graph
+    players = random.sample(ordinary_players, 100)
+    for p in range(0, 100) :
+        # shuffle the players list
+        random.shuffle(players, random.random)
+
+        # Define graph
+        G = nx.cycle_graph(len(players))
+        # K = nx.newman_watts_strogatz_graph(len(players), 4, 0)
+
+        edges = G.edges()
+
+        filename = '/home/c1569433/src/jobs/data/Cycle-{}-{}.csv'.format(seed, p)
+        # in file name the first number is the seed the second in which
+        # shuffle
+        results = results.append([tournament_results(G, seed, p, players, turns,
+                               edges, repetitions, filename)])
+
+results = results[cols]
+results.to_csv(write_out, index=False)
