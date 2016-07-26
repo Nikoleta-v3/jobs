a3bbb6104d5fc45f4140e7e4efe29ea7d5b5da16
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-07-26 12:31:05 +0100

    Make the files more general. Also on the first run it write the head on the csv file and for the rest iterations it does not.

--- scripts/Simple_topology_Lattice.py
+++ scripts/Simple_topology_Lattice.py
@@ -4,26 +4,28 @@ import random
 
 import pandas as pd
 import numpy as np
+import os.path
 
 from functions import *
 from data_structure import *
 
 # parameters
 turns = 200
-repetitions = 100
+repetitions = 10
 ordinary_players = [s() for s in axl.ordinary_strategies]
-
+num_players = 5
 # where to export
-write_out = '/home/c1569433/src/jobs/data/Lattice/50_Players/Lattice.csv'
+write_out = '/scratch/c1569433/data/Lattice_{}_players.csv'.format(num_players)
+file_exists = os.path.isfile(write_out)
 
 results = pd.DataFrame()
-for seed in range(0, 50):
+for seed in range(0, 10):
 
     np.random.seed(seed)
 
     # define the graph
-    players = random.sample(ordinary_players, 50)
-    for p in range(0, 10) :
+    players = random.sample(ordinary_players, num_players)
+    for p in range(0, 5) :
         # shuffle the players list
         random.shuffle(players, random.random)
 
@@ -35,7 +37,13 @@ for seed in range(0, 50):
         #filename = '/home/c1569433/src/jobs/data/Lattice/5_Players/Lattice-{}-{}.csv'.format(seed, p)
         # in file name the first number is the seed the second in which
         # shuffle
+
         results = results.append([tournament_results(G, seed, p, players, turns,
                                edges, repetitions)]) #filename
 
-        results.to_csv(write_out, mode='a',  index=False)
+        results = results[cols]
+
+        if not file_exists:
+            results.to_csv(write_out, index=False, header=True)
+        else :
+            results.to_csv(write_out, mode='a',  index=False, header=False)
