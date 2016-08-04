3fe9969fbf43a74d60181f9e6ea7eed1ceb7efda
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-08-04 20:45:07 +0100

    Test for spatial.
    Running tournaments in raven takes some time thus here I am implementing
    a small test run tournament. It will make sure that error texts are
    exported correctly and data as well.

---
+++ scripts/test.py
@@ -0,0 +1,61 @@
+import axelrod as axl
+import networkx as nx
+import random
+
+import pandas as pd
+import numpy as np
+
+import os.path
+
+from functions import *
+from data_structure import *
+
+# parameters
+turns = 200
+repetitions = 10
+ordinary_players = [s() for s in axl.ordinary_strategies]
+
+for i in range (2) :
+    if i == 0 :
+        num_neighbors = '4'
+        neighbors = 4
+    if i ==1  :
+        num_neighbors = '8'
+        neighbors = 8
+
+    # Title
+    experiment = 'newman_watts_strogatz'
+
+    # where to export
+    write_out = '/scratch/c1569433/data/{}_{}.h5'.format(experiment, num_neighbors)
+    file_exists = os.path.isfile(write_out)
+
+    results = pd.DataFrame()
+    for seed in range(0, 5):
+
+        # set seed
+        axl.seed(seed)
+
+        num_players = random.randint(2, 132)
+        # define the graph
+        players = random.sample(ordinary_players, num_players)
+
+        for parameter in range(0, 10) :
+
+            p = parameter/100
+            # Define graph
+            G = nx.newman_watts_strogatz_graph(len(players), 8, p)
+
+            edges = G.edges()
+
+            results = results.append([tournament_results(G, seed, p, players, turns,
+                                   edges, repetitions)])
+
+            results = results[cols]
+
+            if not file_exists:
+                results.to_hdf(write_out, '{}_{}'.format(experiment, num_neighbors),
+                               index=False, header=True)
+            else :
+                results.to_hdf(write_out, '{}_{}'.format(experiment, num_neighbors),
+                               mode='a',  index=False, header=False)
