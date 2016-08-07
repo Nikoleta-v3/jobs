cf09715d2b0c581e728c3df68a1dc9914ac7c901
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-08-07 16:41:48 +0100

    Corrections. neighborhood_size <- tournament_size initial_neighbors <- initial_neighborhood_size if connections and 1 not in connections <- if connections and (1 not in connections)
    and setting seed for players sample.

--- scripts/Small_world.py
+++ scripts/Small_world.py
@@ -13,14 +13,14 @@ from data_structure import *
 # parameters
 turns = 200
 repetitions = 10
-ub_neighborhood_size = 50
+ub_tournament_size = 50
 ub_seed = 10
 num_sample_players = 10
 ub_parameter = 10
 
 ordinary_players = [s() for s in axl.strategies]
 
-# TitleS
+# titles
 experiment = 'watts_strogatz'
 
 # where to export
@@ -28,31 +28,33 @@ write_out = '/scratch/c1569433/data/{}.h5'.format(experiment)
 file_exists = os.path.isfile(write_out)
 
 results = pd.DataFrame()
-for neighborhood_size in range(2, ub_neighborhood_size):
-    for initial_neighbors in range(1, neighborhood_size):
+for tournament_size in range(2, ub_tournament_size):
+    for initial_neighborhood_size in range(1, tournament_size):
 
         # set seed
-        axl.seed(neighborhood_size)
+        axl.seed(tournament_size)
 
-        for _ in range (num_sample_players)
+        for sample_players in range (num_sample_players)
             # create players
-            players = random.sample(ordinary_players, neighborhood_size)
+            axl.seed(sample_players)
+            players = random.sample(ordinary_players, tournament_size)
 
             for parameter in range(0, ub_parameter) :
+
+                p = parameter/10
                 for seed in range(ub_seed) :
-                    p = parameter/10
+
 
                     axl.seed(seed)
                     # Define graph
                     G = nx.watts_strogatz_graph(len(players),
-                                                           initial_neighbors, p)
+                                                   initial_neighborhood_size, p)
                     # check for unconnected nodes
                     connections = [len(c) for c in
                                    sorted(nx.connected_components(G), key=len,
                                                                   reverse=True)]
 
-
-                    if connections and 1 not in connections:
+                    if connections and 1 not in connections :
 
                             edges = G.edges()
                             results = results.append([tournament_results(G,
@@ -66,6 +68,5 @@ for neighborhood_size in range(2, ub_neighborhood_size):
                                                header=True)
                             else :
                                 results.to_hdf(write_out, '{}'.format(experiment),
-                                               mode='a',  index=False,
+                                               mode='a', index=False,
                                                header=False)
-                            success = True
