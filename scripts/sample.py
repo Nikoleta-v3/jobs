53b67c254f42e3caf0257022f9e57ff09287d31d
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-08-12 15:56:40 +0100

    Comments to work on sample.

--- scripts/sample.py
+++ scripts/sample.py
@@ -9,7 +9,8 @@ data = pd.read_hdf(argv[1])
 # sample from data
 sample_size = int(len(data)*20/100)
 
-sample = data.sample(sample_size)
+#seed here
+sample = data.sample(sample_size, random_state=1)
 
 # output the sample
 sample.to_hdf(argv[2], 'sample')
