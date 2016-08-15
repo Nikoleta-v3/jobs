5d8f86bda8b86391cf5f7a6bfbb20c6718194f86
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-08-15 20:43:54 +0100

    Second generation experiments.
    Making code more efficient. Instead of writting to data frame, appending straight to hdf5 file.

--- scripts/Binomial.py
+++ scripts/Binomial.py
@@ -1,6 +1,8 @@
 import axelrod as axl
 import networkx as nx
 import random
+import tqdm
+import os
 
 import pandas as pd
 import numpy as np
@@ -8,7 +10,6 @@ import numpy as np
 import os.path
 
 from functions import *
-from data_structure import *
 
 # parameters
 turns = 200
@@ -22,13 +23,11 @@ ordinary_players = [s() for s in axl.strategies]
 
 # titles
 experiment = 'binomial'
+# create store
+hdf = pd.HDFStore('/scratch/c1569433/data/{}.h5'.format(experiment))
 
-# where to export
-write_out = '/scratch/c1569433/data/{}.h5'.format(experiment)
-file_exists = os.path.isfile(write_out)
-
-results = pd.DataFrame()
-for tournament_size in range(2, ub_tournament_size):
+tournament_id = 0
+for tournament_size in tqdm.tqdm(range(2, ub_tournament_size + 1)):
 
         # set seed
         axl.seed(tournament_size)
@@ -38,33 +37,35 @@ for tournament_size in range(2, ub_tournament_size):
             axl.seed(sample_players)
             players = random.sample(ordinary_players, tournament_size)
 
-            for parameter in range(0, ub_parameter) :
+            for parameter in range(1, ub_parameter + 1) :
 
-                p = parameter/10
+                p = parameter/(ub_parameter)
                 for seed in range(ub_seed) :
 
                     axl.seed(seed)
                     # Define graph
                     G = nx.binomial_graph(len(players), p)
 
-                    # check for unconnected nodes
+                    # check for connected nodes
                     connections = [len(c) for c in
                                    sorted(nx.connected_components(G), key=len,
                                                                   reverse=True)]
 
-                    if connections and 1 not in connections :
+                    if connections and (1 not in connections):
+                    # if connections is empty, or equal to 1, it means
+                    # at least on node is disconnected. Thus, skip this
+                    # tournament and generate next
+                        edges = G.edges()
 
-                            edges = G.edges()
-                            results = results.append([tournament_results(G,
-                                                        seed, p, players, turns,
-                                                           edges, repetitions)])
-                            results = results[cols]
+                        tournament_id += 1
+                        results = tournament_results(G, seed, p, players, turns,
+                                                             edges, repetitions,
+                                                                  tournament_id,
+                                                                              0)
 
-                            if not file_exists:
-                                results.to_hdf(write_out, '{}'.format(experiment),
-                                               index=False,
-                                               header=True)
-                            else :
-                                results.to_hdf(write_out, '{}'.format(experiment),
-                                               mode='a', index=False,
-                                               header=False)
+                        min_itemsize = {'player_name': 100,
+                                        'neighbors': 200,
+                                        'cliques': 200}
+
+                        hdf.append(experiment, results, format='table', data_columns=True,
+                                   min_itemsize=min_itemsize)
